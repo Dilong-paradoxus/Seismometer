f10343c49cc236c6ca76fd3ed1a979b311074a35
Dilong-paradoxus <32314600+Dilong-paradoxus@users.noreply.github.com>
2019-08-03 17:51:05 -0700

    add accelinfo.py
    replaces coords info file

---
+++ accelinfo.py.py
@@ -0,0 +1,10 @@
+coords = []
+
+def getseiscoords():
+    coords = (47.7477899,-122.526889)
+    return coords
+
+filepath = []
+def getfilepath():
+    filepath = r"D:\Nick\Python\*.csv"
+    return filepath
