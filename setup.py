5013540bdbb09c89ddff0ca94603ddfa3abb0b04
32314600+Dilong-paradoxus <32314600+Dilong-paradoxus@users.noreply.github.com>
2020-06-18 13:56:36 -0700

    setup.py now works, hourly reset on serialread

--- setup.py
+++ setup.py
@@ -6,13 +6,46 @@ print("Welcome! This will generate a new accelinfo.py if you don't already have 
 print("Checking for accelinfo.py...")
 
 workdir = os.getcwd()
+filename = workdir + '\\ConfigFiles\\accelinfo.py'
 print('Current location: ' + workdir)
 
-if os.path.isfile(workdir + '\\ConfigFiles\\accelinfo.py') == false:
-    print('accelinfo.py not found')
-    
+if os.path.isfile(filename) == False:
+    print("accelinfo.py doesn't exist yet, creating it now")
+    with open(filename,"a") as f: #write serial to file
+        f.write('# This file contains some helper functions to store personal configuration data and\n')
+        f.write('# keep it from being uploaded to the git repository along with program files\n')
+        f.write('\n')
 
-elif:
+        #Sensor coordinates: getseiscoords()
+        f.write('def getseiscoords(): #returns the latitude and longitude of the seismometer\n')
+
+        usr_input = input('Please type the latitude of your sensor here: ')
+        f.write('    your_lat = ' + usr_input + '\n')
+        usr_input = input('Please type the longitude of your sensor here: ')
+        f.write('    your_long = ' + usr_input + '\n')
+
+        f.write('    coords = (your_lat,your_long)\n')
+        f.write('    return coords\n')
+        f.write('\n')
+
+        #Filepath: getfilepath()
+        f.write('def getfilepath(): #returns the filepath where csv files are stored\n')
+        f.write('    filepath = r"' + workdir + '\\AccelerometerData\\"\n')
+        f.write('    return filepath\r\n')
+        f.write('\n')
+
+        #Serial port: getCOMnumber()
+        f.write('def getCOMnumber(): #returns the serial port ID of the seismometer\n') 
+
+        usr_input = input(r'The serial port of your sensor looks like \dev\tty\ACM0 on linux or COM4 on windows.' 
+            + '\nPlease type it here: ')
+        f.write("    COM_port = '"+ usr_input + "'\n")
+        f.write("    return COM_port\n")
+
+    print('All done! Feel free to modify this file later if necessary.')
+    #filename.close()
+
+elif os.path.isfile(filename) == True:
     print('accelinfo.py already exists!')
     print('If you need to change something, just open it in your favorite text editor.')
     print('If it is broken somehow, delete or move it and rerun this file.')
