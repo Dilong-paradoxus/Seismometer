bb684365591c54abab03c89dd8a798b1ec62e823
32314600+Dilong-paradoxus <32314600+Dilong-paradoxus@users.noreply.github.com>
2020-06-18 12:14:06 -0700

    fixed saving, made setup, changed accelinfo

--- ConfigFiles/accelinfo.py
+++ ConfigFiles/accelinfo.py
@@ -1,10 +1,16 @@
-coords = []
+# This file contains some helper functions to store personal configuration data and
+# keep it from being uploaded to the git repository along with program files
 
-def getseiscoords():
-    coords = (lat,long)
+def getseiscoords(): #returns the latitude and longitude of the seismometer
+    your_lat = 47.7477899
+    your_long = -122.526889
+    coords = (your_lat,your_long)
     return coords
 
-filepath = []
-def getfilepath():
+def getfilepath(): #returns the filepath where csv files are stored
     filepath = r"C:\yourpath\*.csv"
     return filepath
+
+def getCOMnumber(): #returns the serial port ID of the seismometer
+    COM_port = 'COM4'
+    return COM_port
